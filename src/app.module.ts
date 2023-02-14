1123cca4669a35ae07ed0863ab10de1672696067
AhmedHdeawy <ahmedhdeawy@gmail.com>
2023-02-14 08:33:32 +0200

    Connect to MySQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,22 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
-
+import { DatabaseModule } from './database/database.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriesModule } from './app/categories/categories.module';
-// import dbConfiguration from './config/database.config';
+import { DataSource } from 'typeorm';
 
 @Module({
   imports: [
     CategoriesModule,
+    ConfigModule.forRoot(),
+    DatabaseModule
 
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource){}
+}
