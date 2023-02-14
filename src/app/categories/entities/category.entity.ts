ab93f539fe369235a26843acd90c88a5ce3b39dd
AhmedHdeawy <ahmedhdeawy@gmail.com>
2023-02-14 09:58:30 +0200

    Make Generic APi Response

--- src/app/categories/entities/category.entity.ts
+++ src/app/categories/entities/category.entity.ts
@@ -12,9 +12,9 @@ export class Category {
     @Column("text")
     description: string;
 
-    @CreateDateColumn({ type: "timestamp", default: () => "CURRENT_TIMESTAMP(6)" })
-    public created_at: Date;
+    @CreateDateColumn()
+    created_at: Date;
 
-    @UpdateDateColumn({ type: "timestamp", default: () => "CURRENT_TIMESTAMP(6)", onUpdate: "CURRENT_TIMESTAMP(6)" })
-    public updated_at: Date;
+    @UpdateDateColumn()
+    updated_at: Date;
 }
