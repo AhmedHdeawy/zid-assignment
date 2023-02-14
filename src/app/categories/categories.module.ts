ee442c240e695c82fb8818f4ee2dbfdf1568a411
AhmedHdeawy <ahmedhdeawy@gmail.com>
2023-02-14 08:47:31 +0200

    Add Category Repository

--- src/app/categories/categories.module.ts
+++ src/app/categories/categories.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
+import { Category } from './entities/category.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Category])],
   controllers: [CategoriesController],
   providers: [CategoriesService]
 })
