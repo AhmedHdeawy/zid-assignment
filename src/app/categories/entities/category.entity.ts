8d0aeb5a372956593fd6dd2c610334661a89ab52
AhmedHdeawy <ahmedhdeawy@gmail.com>
2023-02-14 08:33:51 +0200

    Category Schema

--- src/app/categories/entities/category.entity.ts
+++ src/app/categories/entities/category.entity.ts
@@ -3,5 +3,18 @@ import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateCol
 @Entity()
 export class Category {
 
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column("varchar", { length: 200 })
+    name: string;
     
+    @Column("text")
+    description: string;
+
+    @CreateDateColumn({ type: "timestamp", default: () => "CURRENT_TIMESTAMP(6)" })
+    public created_at: Date;
+
+    @UpdateDateColumn({ type: "timestamp", default: () => "CURRENT_TIMESTAMP(6)", onUpdate: "CURRENT_TIMESTAMP(6)" })
+    public updated_at: Date;
 }
