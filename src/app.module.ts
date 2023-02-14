5158c234022af3b2ae082bd7c56ccabcd9e3becc
AhmedHdeawy <ahmedhdeawy@gmail.com>
2023-02-14 07:30:01 +0200

    Create categories resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategoriesModule } from './app/categories/categories.module';
+// import dbConfiguration from './config/database.config';
 
 @Module({
-  imports: [],
+  imports: [
+    CategoriesModule,
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
