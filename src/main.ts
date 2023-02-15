a2b1174ec677c45e6924bb4c48206a087d8d7fa3
AhmedHdeawy <ahmedhdeawy@gmail.com>
2023-02-15 09:59:33 +0200

    Add Global Prefix

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
+  app.setGlobalPrefix('api/v1');
+
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
 
   await app.listen(3200);
