e5d01498b59ec93abab71fb9bb5f881bef70d13c
AhmedHdeawy <ahmedhdeawy@gmail.com>
2023-02-15 10:28:08 +0200

    Add Category to products entity

--- src/app/products/entities/product.entity.ts
+++ src/app/products/entities/product.entity.ts
@@ -1,20 +1,33 @@
-import { Entity, Column, Unique, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { Category } from 'src/app/categories/entities/category.entity';
+import { Entity, Column, DeleteDateColumn, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, ManyToOne, JoinColumn } from 'typeorm';
 
 @Entity()
 export class Product {
-
-    @PrimaryGeneratedColumn()
+    @PrimaryGeneratedColumn({ unsigned: true })
     id: number;
 
     @Column("varchar", { length: 200, unique: true })
     name: string;
+
+    @Column("decimal", { precision: 6, scale: 2 })
+    price: number;
     
     @Column("text")
     description: string;
+
+    @Column({ unsigned: true })
+    category_id: number;
+
+    @ManyToOne(() => Category, category => category.id)
+    @JoinColumn({ name: 'category_id' })
+    category: Category;
 
     @CreateDateColumn()
     created_at: Date;
 
     @UpdateDateColumn()
     updated_at: Date;
+
+    @DeleteDateColumn()
+    deleted_at?: Date;
 }
